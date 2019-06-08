f2c3509e9f6a28e6c4045a60f9965fbf573524b4
Gabriel Carraro <gacarraro@gmail.com>
2019-06-08 19:16:46 -0300

    cast to binary array works

--- raw-socket/messenger.c
+++ raw-socket/messenger.c
@@ -6,13 +6,24 @@ unsigned char *string_to_bin_array(char *string){
 	int array_size = string_size * sizeof(char);
 	unsigned char *bin_array[array_size];
 
-	for (int i = 0; i < string_size; i++){
-		char current_char = string[i];
-		printf("%c - ", current_char);
-		for (int j = 0; j < 8; j++){
-		    printf("%d", current_char & (1 << (8 - j)));	
-		}
-		puts();
+	// for (int i = 0; i < string_size; i++){
+	// 	char current_char = string[i];
+	// 	printf("%c - ", current_char);
+	// 	for (int j = 0; j < 8; j++){
+	// 	    printf("%d", (unsigned char)current_char);	
+	// 	}
+	// 	puts();
+	// }
+
+	for (char *temp = string; *temp; ++temp){
+	    int bit_index;
+	    printf("%c - ", *temp);
+	    for (bit_index = 7; bit_index >= 0; --bit_index){
+	        int bit = *temp >> bit_index & 1;
+	        printf("%d", bit);
+	    }
+	    
+	    printf("\n");
 	}
 }
 
