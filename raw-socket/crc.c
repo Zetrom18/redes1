7dda0d695de3dacb12b06c57160fe9955ae5c9e9
Lucas Rosa <luc1604@hotmail.com>
2019-06-16 17:27:30 -0300

    finished crc calculator
    Signed-off-by: Lucas Rosa <luc1604@hotmail.com>

---
+++ raw-socket/crc.c
@@ -0,0 +1,29 @@
+#include "crc.h"
+
+int _CRC_DIVISOR[CRC_SIZE+1] = {1,1,0,1,1,0,1,1,0};
+
+void calculate_crc(int* data, int data_size, int* crc){
+  int i, j;
+  int *buffer = (int*)malloc(sizeof(int)*(data_size+CRC_SIZE));
+
+  for(i=0; i<data_size; i++){ // copia data para buffer
+    buffer[i] = data[i];
+  }
+  for(j=0; j<CRC_SIZE; j++){ // copia crc para bufefr
+    buffer[i+j] = crc[j];
+  }
+
+  for(i=0; i<data_size; i++){
+    if(buffer[i]!=0){
+      for(j=0; j<CRC_SIZE; j++){
+        buffer[i+j] = (buffer[i+j] + _CRC_DIVISOR[j]) % 2;
+      }
+    }
+  }
+
+  for(j=0; j<CRC_SIZE; j++){  // copia crc calculado
+    crc[j] = buffer[i+j];
+  }
+
+  free(buffer);
+}
---
+++ raw-socket/crc.h
@@ -0,0 +1,12 @@
+#ifndef _CRC_H_
+#define _CRC_H_
+
+#ifndef CRC_SIZE
+#define CRC_SIZE 8
+#endif
+
+#include <stdlib.h>
+
+void calculate_crc(int *, int, int *);
+
+#endif
