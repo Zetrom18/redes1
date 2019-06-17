cff927954422f9f5024bf2d39988e49d6c800c69
Lucas Rosa <luc1604@hotmail.com>
2019-06-16 23:13:34 -0300

    fewrfc
    Signed-off-by: Lucas Rosa <luc1604@hotmail.com>

--- raw-socket/common.c
+++ raw-socket/common.c
@@ -89,8 +89,8 @@ void clearCommands(char **cg){
 }
 
 void showbits(unsigned int x) {
-    for(int i = (sizeof(char)*92) - 1; i >= 0; i--) {
+    for(int i = (sizeof(char)*8) - 1; i >= 0; i--) {
        putchar(x & (1u << i) ? '1' : '0');
     }
-    printf("\n");
+    printf(" ");
 }
