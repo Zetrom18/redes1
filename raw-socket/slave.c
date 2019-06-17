ce813bee650b51a7a482c3cda5e496e0129462f2
Lucas Rosa <luc1604@hotmail.com>
2019-06-16 23:17:25 -0300

    fewfew
    Signed-off-by: Lucas Rosa <luc1604@hotmail.com>

--- raw-socket/slave.c
+++ raw-socket/slave.c
@@ -42,7 +42,7 @@ void init() {
 }
 
 void controller(int socket){
-  unsigned char buffer[10];
+  unsigned char buffer[1024];
   int count = 0;
   int i, len = 0;
   while (true) {
@@ -54,7 +54,7 @@ void controller(int socket){
       printf("\nMessage %d (size %d):\n", count, len);
       for(i=0; i<len; i++){
         showbits(buffer[i]);
-        puts("");
+        printf(" ");
       }
       // printf("%02x", buffer[i]);
       // puts("");
